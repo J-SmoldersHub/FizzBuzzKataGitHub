6116df92d307b906ff406526fab41c56355666af
Joris Smolders <joris_smolders@outlook.com>
2022-01-17 11:31:07 +0100

    green

--- FizzBuzzKataGitHub/Converter.cs
+++ FizzBuzzKataGitHub/Converter.cs
@@ -4,15 +4,22 @@
     {
         private readonly string Hyphen = "-";
         private readonly string Fizz = "Fizz";
+        private readonly string Buzz = "Buzz";
+        private readonly string FizzBuzz = "FizzBuzz";
 
         public string Convert(int input)
         {
-            return IsDivisableByThree(input)
-                ? Fizz
-                : Hyphen;
+            return IsDivisableByFive(input)
+                ? Buzz
+                : IsDivisableByThree(input)
+                    ? Fizz
+                    : Hyphen;
         }
 
         private bool IsDivisableByThree(int input) =>
             input % 3 == 0;
+
+        private bool IsDivisableByFive(int input) =>
+            input % 5 == 0;
     }
 }
