0e88c2ae3d0ef89c58a27acf52cabdcd9cfa1da7
Joris Smolders <joris_smolders@outlook.com>
2022-01-17 11:29:37 +0100

    red

--- FizzBuzzKataGitHubTests/FizzBuzzTests.cs
+++ FizzBuzzKataGitHubTests/FizzBuzzTests.cs
@@ -6,6 +6,8 @@ namespace FizzBuzzKataGitHubTests
     {
         private readonly string Hyphen = "-";
         private readonly string Fizz = "Fizz";
+        private readonly string Buzz = "Buzz";
+        private readonly string FizzBuzz = "FizzBuzz";
 
         [Fact]
         public void InputIsOne_ShouldReturnHyphen()
@@ -31,6 +33,19 @@ namespace FizzBuzzKataGitHubTests
 
             // Assert
             Assert.Equal(Fizz, output);
+        }
+
+        [Fact]
+        public void InputIsFive_ShouldReturnBuzz()
+        {
+            // Arrange
+            var c = new FizzBuzzKataGitHub.Converter();
+
+            // Act
+            string output = c.Convert(5);
+
+            // Assert
+            Assert.Equal(Buzz, output);
         }
     }
 }
